50517ce36517fff3ff8b5f67cb8afaf57cce5d0b
endlesslit <dinfar1234er@gmaiil.com>
2025-05-03 17:24:16 +0200

    Add join redirect handler

--- pages/join.js
+++ pages/join.js
@@ -1,21 +1,11 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+export default function handler(req, res) {
+  const { placeId, jobId } = req.query;
 
-export default function JoinRedirect() {
-  const router = useRouter();
-  const { placeId, jobId } = router.query;
+  if (!placeId || !jobId) {
+    return res.status(400).send('Missing placeId or jobId');
+  }
 
-  useEffect(() => {
-    if (placeId && jobId) {
-      const robloxLink = `roblox://placeId=${placeId}&gameInstanceId=${jobId}`;
-      window.location.href = robloxLink;
-    }
-  }, [placeId, jobId]);
-
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '3em' }}>
-      <h2>Redirecting you to Roblox...</h2>
-      <p>If nothing happens, make sure you have Roblox installed.</p>
-    </div>
-  );
+  const robloxLink = `roblox://placeId=${placeId}&gameInstanceId=${jobId}`;
+  res.writeHead(302, { Location: robloxLink });
+  res.end();
 }
